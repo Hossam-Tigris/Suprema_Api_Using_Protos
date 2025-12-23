c93f0ad414d55bae0826b1461187871787fd3be6
Hossam.H <hossam.hassan@tigrisnet.tech>
2025-12-23 10:32:39 +0200

    CreateScheduleService

---
+++ Suprema_Api_Using_Protos/InterFaces/IScheduleService.cs
@@ -0,0 +1,10 @@
+﻿using Gsdk.Schedule;
+
+namespace Suprema_Api_Using_Protos.InterFaces
+{
+    public interface IScheduleService
+    {
+        Task AddWeeklyAsync(uint deviceId, uint scheduleId, string name, Dictionary<int, List<(int start, int end)>> days);
+        Task<GetListResponse> GetListAsync(uint deviceId);
+    }
+}
